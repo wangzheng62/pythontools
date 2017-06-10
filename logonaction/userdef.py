0613cddea696f3ffeb9c4d57026d53728a6ccf3e
wangzheng62 <363579018@qq.com>
2017-06-10 14:43:38 +0800

    55

--- logonaction/userdef.py
+++ logonaction/userdef.py
@@ -1,6 +1,12 @@
+def mssqlgetcol(**kw):
+	pass
+def mysqlgetcol(**kw):
+	pass
 class TableMetaclass(type):
 	def __new__(cls,name,bases,attrs):
 		print(attrs)
+		print(bases)
+		print(name)
 		if attrs['dbtype']=='mssql':
 			attrs['__slot__']=mssqlgetcol(db=attrs['db'],tablename=attrs['tablename'])
 		if attrs['dbtype']=='mysql':
@@ -15,3 +21,19 @@ class Table(dict,metaclass=TableMetaclass):
 if __name__=='__main__':
 	t=Table()
 	print(t.__slot__)
+	a=1
+	b=1
+	if a is b:
+		print(1)
+	c=[]
+	d=[]
+	if c is b:
+		print('[]')
+	n='123456789'
+	print(n[0])
+	bt=bytes('abc','utf8')
+	print(bt)
+	s1='abc'
+	bt1=s1.encode()
+	print(bt1)
+	print(bt.decode())
